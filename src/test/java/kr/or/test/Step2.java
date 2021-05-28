65f584561c61eec90228e2ec9649d693c644efde
User <User@DESKTOP-VNGL1B2>
2021-05-28 14:48:03 +0900

    커밋

--- src/test/java/kr/or/test/Step2.java
+++ src/test/java/kr/or/test/Step2.java
@@ -1,15 +1,15 @@
 package kr.or.test;
 /**
- * 이 클래스는 메서드 기반의 Step1클래스를 객체 기반의 클래스로 변경한 클래스.
+ * 이 클래스는 메서드기반의 Step1클래스를 객체기반의 클래스로 변경한 클래스.
  * @author 조민재
  *
  */
 class MemberVO {
-	//이 클래스는 회원정보를 저장하는 자료형 클래스(자료)입니다.
+	//이 클래스는 회원정보를 저장하는 클래스(자료) 입니다.
 	private String name;
 	private int age;
 	private String phoneNum;
-	//위 프라이빗 멤버변수를 입출력을 구현 메서드를 만듭니다.
+	//위 프라이빗 멤버변수를 입출력을 구현한 메서드를 만듭니다.
 	public String getName() {
 		return name;
 	}
@@ -22,20 +22,21 @@ class MemberVO {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	// String을 반환(return)=출력하는 겟 메서드입니다(아래)
+	// String을 반환(return)=출력하는 겟 메서드 입니다.(아래)
 	public String getPhoneNum() {
 		return phoneNum;
 	}
-	// void한(반환값이 없이) 입력하는 셋 메서드입니다.(아래)
+	// void한(반환값이 없이) 입력한는 셋 메서드 입니다.(아래)
 	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;//프라이빗 변수를 직접 접근을 못하니까 퍼블릿 메서드로 입력하게 만들어 놓은 구조입니다.
+		this.phoneNum = phoneNum;//프라이빗변수를 직접접근을 못하니까 
+		//퍼블릿 메서드로 입력하게 만들어 놓은 구조입니다. 
 	}
 	
 }
 public class Step2 {
 
 	public static void main(String[] args) {
-		// MemberVO클래스 자료형(회원정보) 객체를 생성합니다.(아래)
+		// MemberVO클래스자료형(회원정보) 객체를 생성합니다(아래)
 		MemberVO memberVO = new MemberVO();
 		memberVO.setName("홍길동");
 		memberVO.setAge(10);
@@ -56,17 +57,27 @@ public class Step2 {
 		//출력도 메서드를 바로 호출하지 않고, 외부 클래스에서 객체로 만들어서 메서드를 호출합니다.
 		MemberService memberService = new MemberService();
 		memberService.printMember(members);
+		//객체로 만들면, 호출 (runtime)시 메모리에 로딩됩니다. -> 실행 끝나면, 반환
+		//클래스는 아래처럼 직접 접근해서 메서드나 변수를 사용할 수 없음.
+		//외부 클래스로 접근하려면, 객체(실행 가능한=메모리로딩)로 만들어야 가능함.
+		//Static 예약어는 컴파일시 메모리에 로딩(객체가됨)이 되게 명시
+		//매번 Static으로 만들면 메모리가 가득차서 프로그램 실행이 느려지거나 멈춤
+		//MemberService.printMember(members);
+		memberService = null;//메모리에서 오브젝트를 삭제하는 명령
+
 	}
 }
 class MemberService {
 
 	public void printMember(MemberVO[] members) {
-		// 멤버 서비스 클래스에서 퍼블릿 접근 제어로 회원 정보를 출력하는 메서드 생성
+		// 멤버서비스클래스에서 퍼블릿 접근제어로 회원정보를 출력하는 메서드 생성
 		// 향상된 for문을 사용합니다.
 		for(MemberVO member:members) {
-			System.out.println("회원의 이름은 "+member.getName()+" 나이는"+member.getAge()+" 폰번호는 "+member.getPhoneNum());
+			System.out.println("회원의 이름은 "+member.getName()
+			+" 나이는 "+member.getAge()
+			+" 폰번호는 "+member.getPhoneNum());
 		}
 	}
-	//회원을 출력하는 메서드를 생성합니다.
+	//회원을 출력하는 메서드를 생성 합니다.
+	
 }
-
