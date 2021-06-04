546f15c20e225c6f2f94cc5ad2db16cdeebf34cd
user <user@LAPTOP-F6UFT8DV>
2021-06-04 11:39:45 +0900

    3교시 커밋

--- DATABASES/sql진도_20210603.sql
+++ DATABASES/sql진도_20210603.sql
@@ -33,7 +33,14 @@ SELECT * FROM emp WHERE comm is NULL;
 SELECT DECODE(comm,100,0), E.* FROM emp E WHERE NVL(comm,0) = 0;
 -- NVL2(필드명,널이아닐때100,널일때0), NVL(필드명,널일때0)
 -- 오라클은 표준쿼리X ANSI쿼리 표준O 입니다.
-SELECT DECODE(comm,null,0,100),NVL2(comm,100,0), E.* FROM emp E WHERE NVL(comm,0) = 0;
+SELECT
+CASE WHEN comm is null THEN 0
+WHEN comm = 0 THEN 100
+WHEN comm > 0 THEN comm
+END AS "CASE출력문"
+, DECODE(comm,null,0,100)
+,NVL2(comm,100,0)
+, E.* FROM emp E; --WHERE NVL(comm,0) = 0;
 -- 연봉 기준으로 정렬 sort, = 순서 order by 필드명 오름차순[초기값]|내림차순
 -- (중요)서브쿼리? (select쿼리가 중복되어있는...) 입니다. 
 SELECT ROWNUM, E.* FROM (--테이블명
