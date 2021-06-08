d76a3eb50c9092b88eacb936d35e8a3b744379ed
user <user@LAPTOP-F6UFT8DV>
2021-06-08 14:17:22 +0900

    5교시 중간 커밋

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -15,6 +16,9 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.edu.service.IF_MemberService;
+import com.edu.vo.MemberVO;
 
 /**
  * 이 클래스는 오라클과 연동해서 CRUD를 테스트하는 클래스 입니다.
@@ -34,6 +38,8 @@ public class DataSourceTest {
 	@Inject //인젝트는 스프링에서 객체를 만드는 방법, 이전 자바에서는 new 키워드로 객체를 만들었고... 
 	DataSource dataSource;//Inject로 객체를 만들면 메모리 관리를 스프링이 대신해 줌.
 	//Inject 자바8부터 지원, 그럼, 이전 자바7에서 @Autowired 로 객체를 만들었슴
+	@Inject //MemberService서비스를 주입받아서 객체를 사용합니다.(아래)
+	private IF_MemberService memberService;
 	
 	//스프링 코딩 시작 순서
 	// M-V-C 사이에 데이터를 입출력 하는 임시 저장 공간(VO클래스-멤버변수+Get/Set메서드) 생성
@@ -43,7 +49,8 @@ public class DataSourceTest {
 	@Test
 	public void selectMember() throws Exception {
 		//회원관리 테이블에서 더미로 입력한 100개의 레코드를 출력 메서드 테스트->회원관리 목록이 출력
-		
+		List<MemberVO> listMember = memberService.selectMember();
+		listMember.toString();
 	}
 	
 	@Test
