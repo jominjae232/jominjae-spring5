ce515781aa734dd7129812315a8d3ef6c2c4fca9
user <user@LAPTOP-F6UFT8DV>
2021-06-08 16:40:47 +0900

    7교시 커밋

---
+++ src/main/java/com/edu/vo/PageVO.java
@@ -0,0 +1,21 @@
+package com.edu.vo;
+/**
+ * 이 클래스는 공통(회원관리, 게시물 관리)으로 사용하는 페이징처리+검색기능의 클래스
+ * @author 조민재
+ * 이 클래스는 오라클이든, MySql(마리아DB) 어디서든 공통으로 사용 Get/Set.
+ * 페이징에 사용되는 변수(쿼리변수+VO변수) 아래
+ * queryStarNo, queryPerPageNum, page, perPageNum, startPage, endPage
+ * 검색에 사용되는 변수(쿼리변수만): 검색어(search_keyword), 검색조건(search_type)
+ */
+
+public class PageVO {
+	private int queryStartNo;//쿼리 전용 변수
+	private int queryPerPageNum;//쿼리 전용 변수
+	private Integer page;//jsp에서발생 자바전용
+	private int perPageNum;//UI 하단에 보여줄 페이징 갯수 계산에 필요
+	private int startPage;//위 perPageNum으로 구하는 UI하단 페이지 시작 번호
+	private int endPage;//위 perPageNum으로 구하는 UI하단 페이지끝 번호
+	
+	private String search_keyword;//jsp에서 받은 검색어 쿼리 전용 변수
+	private String search_type;//검색조건에 해당 쿼리전용 변수
+}
