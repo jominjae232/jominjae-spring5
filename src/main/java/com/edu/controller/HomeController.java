9976d4d9df1a1b36400a0fffef54649fd13183e0
user <user@LAPTOP-F6UFT8DV>
2021-07-02 14:40:45 +0900

    커밋

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -45,6 +45,10 @@ public class HomeController {
 	//회원가입 처리 호출 POST방식
 	@RequestMapping(value="/join", method=RequestMethod.POST)
 	public String join(MemberVO memberVO, RedirectAttributes rdat) throws Exception {
+		//rawPassword암호를 스프링시큐리티로 인코딩합니다.(아래)
+		String rawPassword = memberVO.getUser_pw();
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		memberVO.setUser_pw(passwordEncoder.encode(rawPassword));//암호화 실행
 		memberService.insertMember(memberVO);
 		rdat.addFlashAttribute("msg", "회원가입");//회원가입 가(이) 성공했습니다. 출력
 		return "redirect:/login_form";//페이지 리다이렉트로 이동
