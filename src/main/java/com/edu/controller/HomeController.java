d357b231f1477ca02f0ef29bdcfff11a1b984cc6
user <user@LAPTOP-F6UFT8DV>
2021-07-13 11:33:30 +0900

    사용자단 메인 겔러리 수정

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -318,7 +318,19 @@ public class HomeController {
 		pageVO.setPage(1);//필수값 1
 		pageVO.setQueryPerPageNum(3);//겔러리3개, 공지사항5개
 		pageVO.setBoard_type("gallery");
-		model.addAttribute("latestGallery", boardService.selectBoard(pageVO));//겔러리 최근 게시물
+		//첨부파일 save_file_names 배열 변수 값을 지정
+		List<BoardVO> latestGallery = boardService.selectBoard(pageVO);
+				
+		for(BoardVO boardVO:latestGallery) {//리스트형 객체를 1개씩 뽑아서 1개 레코드에 입력 반복
+			List<AttachVO> listAttachVO = boardService.readAttach(boardVO.getBno());			
+			if(listAttachVO.size() > 0) {
+			String[] save_file_names = new String[listAttachVO.size()];
+			save_file_names[0] = listAttachVO.get(0).getSave_file_name();
+			boardVO.setSave_file_names(save_file_names);
+			}
+		}
+		
+		model.addAttribute("latestGallery", latestGallery);//겔러리 최근 게시물
 		
 		pageVO.setQueryPerPageNum(5);//공지사항5개, 보드타입 필요(세션으로 처리 않됨)
 		pageVO.setBoard_type("notice");
