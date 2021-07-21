fa8bcda2014950ae61c0e2506c917f83db14b40b
user <user@LAPTOP-F6UFT8DV>
2021-07-21 17:43:15 +0900

    자바 커밋

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//문자열 변수 하나 선언하고,
+		//자기이름(영문) 변수에 저장한 후
+		//그 변수안의 문자열을 출력한다.
+		String strName = "Jo Min Jae";
+		System.out.println(strName);
+	}
+
+}
