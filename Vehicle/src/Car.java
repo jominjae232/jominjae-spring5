3e8b9a0adab5d4e3c17be810b4148172b557d26b
user <user@LAPTOP-F6UFT8DV>
2021-07-28 17:43:37 +0900

    자바 커밋

---
+++ Vehicle/src/Car.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Car {
+	// 속성=변수=필드(Java)/멤버변수(C++)
+	String nColor;
+	int maxSpeed;
+	int nSpeed;
+	int guest;
+	String modelName;
+	int nWheel;
+	ArrayList<Integer> dailyRun;
+	// 동작=메소드(함수)=메소드(Java)/멤버함수(C++)
+}
---
+++ Vehicle/src/Phon.java
@@ -0,0 +1,7 @@
+
+public class Phon {
+	int nMemory; //메모리 용량
+	int nco;//코어갯수
+	String charging;
+	String screen;
+}
